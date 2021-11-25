e17fd60ea8f6c21cf0b330a27eb6c7e158cbe505
Gabriel0w <gabriel0w@gabrielo.dzn@gmail.com>
2021-11-25 15:46:30 -0300

    Heroku Deploy

--- CondominiumControl/settings.py
+++ CondominiumControl/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +153,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_on_heroku.settings(locals())
